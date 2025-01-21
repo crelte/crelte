174bc24ccd8c460c4984ce192749040384acbe45
Sören Meier <soeren@s-me.ch>
2025-01-21 02:39:21 +0100

    tmp 2

--- packages/node/src/server.ts
+++ packages/node/src/server.ts
@@ -1,8 +1,8 @@
 // import path from 'path';
 import { readFile as readFileAsync } from 'fs/promises';
-import path from 'path';
-import express, { Express } from 'express';
-import { ViteDevServer } from 'vite';
+import { Connect, ViteDevServer } from 'vite';
+import { Readable } from 'node:stream';
+import * as http from 'node:http';
 
 async function readFile(path: string): Promise<string> {
 	// maybe not necessary
@@ -27,147 +27,94 @@ setup vite
 setup route
 */
 
-/**
- * Express Server
- */
-export class CoreServer {
+export type EnvData = {
 	env: Map<string, string>;
-	ssrManifest: Record<string, string>;
-	fileExtension: string;
+	endpointUrl: string;
+	craftWebUrl: string;
+	viteEnv: Map<string, string>;
+};
 
-	endpointUrl!: string;
-	craftWebUrl!: string;
-	viteEnv!: Map<string, string>;
+async function initEnvData(): Promise<EnvData> {
+	const envPath = '../craft/.env';
 
-	/**
-	 * Creates a new Server
-	 */
-	constructor(opts: ServerOptions) {
-		this.env = new Map();
-
-		this.ssrManifest = {};
-
-		this.fileExtension = opts.ts ? 'ts' : 'js';
+	let env;
+	try {
+		env = readEnv(await readFile(envPath));
+	} catch (_e) {
+		throw new Error('failed to read ' + envPath + ' file');
 	}
 
-	/**
-	 * Setups the Express server
-	 *
-	 * - Creates the vite middleware
-	 * - loads the ssr manifest and the index.html file
-	 */
-	async _setup() {
-		const envPath = '../craft/.env';
+	const endpointUrl = env.get('ENDPOINT_URL');
+	if (!endpointUrl) throw new Error('ENDPOINT_URL not set');
 
-		try {
-			this.env = readEnv(await readFile(envPath));
-		} catch (_e) {
-			throw new Error('failed to read ' + envPath + ' file');
-		}
+	const craftWebUrl = env.get('CRAFT_WEB_URL');
+	if (!craftWebUrl) throw new Error('CRAFT_WEB_URL not set');
 
-		const endpointUrl = this.env.get('ENDPOINT_URL');
-		if (!endpointUrl) throw new Error('ENDPOINT_URL not set');
-		this.endpointUrl = endpointUrl;
+	// parse all vite related env variables
+	const viteEnv = new Map(
+		Array.from(env).filter(([key]) => key.startsWith('VITE_')),
+	);
 
-		const craftWebUrl = this.env.get('CRAFT_WEB_URL');
-		if (!craftWebUrl) throw new Error('CRAFT_WEB_URL not set');
-		this.craftWebUrl = craftWebUrl;
-
-		// parse all vite related env variables
-		this.viteEnv = new Map(
-			Array.from(this.env).filter(([key]) => key.startsWith('VITE_')),
-		);
-	}
-
-	// /**
-	//  * Setups a custom route
-	//  *
-	//  * the handler should be a function which will be called from the server.js / .ts
-	//  * file
-	//  */
-	// async register(route: any, handler: string) {
-	// 	this.app.use(route, async (...args) => {
-	// 		const server = await this._getServer();
-	// 		if (!(handler in server))
-	// 			throw new Error('handler ' + handler + ' not found');
-	// 		return await server[handler](...args);
-	// 	});
-	// }
-
-	// /**
-	//  * Start the express server
-	//  *
-	//  * If `process.env.PORT` exists uses it instead.
-	//  *
-	//  * @param  [port=8080]
-	//  * @param  [addr='127.0.0.1']
-	//  */
-	// listen(port: number = 8080, addr: string = '127.0.0.1') {
-	// 	if (process?.env?.PORT) {
-	// 		port = parseFloat(process.env.PORT);
-	// 	}
-
-	// 	if (process?.env?.HOST) {
-	// 		addr = process.env.HOST;
-	// 	}
-
-	// 	console.log('listening on http://' + addr + ':' + port + '/');
-	// 	this.app.listen(port, addr);
-	// }
-
-	async serverModRender(
-		serverMod: any,
-		url: string,
-		htmlTemplate: string,
-		acceptLang: string | null,
-		cookieHeader: string,
-	): Promise<RenderResponse> {
-		return await serverMod.render({
-			url,
-			htmlTemplate,
-			ssrManifest: this.ssrManifest ?? {},
-			acceptLang,
-			endpoint: this.endpointUrl,
-			craftWeb: this.craftWebUrl,
-			viteEnv: this.viteEnv,
-			cookies: cookieHeader,
-		});
-	}
-
-	async serverModRenderError(
-		serverMod: any,
-		error: any,
-		url: string,
-		htmlTemplate: string,
-		acceptLang: string | null,
-	): Promise<RenderResponse> {
-		return await serverMod.renderError(error, {
-			url,
-			htmlTemplate,
-			ssrManifest: this.ssrManifest ?? {},
-			acceptLang,
-			endpoint: this.endpointUrl,
-			craftWeb: this.craftWebUrl,
-			viteEnv: this.viteEnv,
-		});
-	}
+	return {
+		env,
+		endpointUrl,
+		craftWebUrl,
+		viteEnv,
+	};
 }
 
-export async function serveVite(server: CoreServer, vite: ViteDevServer) {
+export type RenderRequest = {
+	url: string;
+	htmlTemplate: string;
+	ssrManifest: Record<string, string>;
+	acceptLang: string | null;
+	endpoint: string;
+	craftWeb: string;
+	viteEnv: Map<string, string>;
+	cookies: string;
+};
+
+async function modRender(
+	mod: any,
+	data: RenderRequest,
+): Promise<RenderResponse> {
+	return await mod.render(data);
+}
+
+export type RenderErrorRequest = {
+	url: string;
+	htmlTemplate: string;
+	ssrManifest: Record<string, string>;
+	acceptLang: string | null;
+	endpoint: string;
+	craftWeb: string;
+	viteEnv: Map<string, string>;
+};
+
+async function modRenderError(
+	mod: any,
+	error: any,
+	data: RenderErrorRequest,
+): Promise<RenderResponse> {
+	return await mod.renderError(error, data);
+}
+
+export async function serveVite(vite: ViteDevServer) {
+	const env = await initEnvData();
+
 	vite.middlewares.use(async (req, res, next) => {
 		const url = req.originalUrl;
 		const protocol = vite.config.server.https ? 'https' : 'http';
 		const fullUrl = protocol + '://' + req.headers['host'] + url;
 		const acceptLang = req.headers['accept-language'] ?? null;
 
+		// todo at this point we should check the routes for overrides
+
 		let thrownError: any = null;
 
-		const serverMod = await vite.ssrLoadModule(
-			'./src/server.' + server.fileExtension,
-			{
-				fixStacktrace: true,
-			},
-		);
+		const serverMod = await vite.ssrLoadModule('./src/server.js', {
+			fixStacktrace: true,
+		});
 
 		let template = await readFile('./index.html');
 		template = await vite.transformIndexHtml(url ?? '', template);
@@ -175,14 +122,21 @@ export async function serveVite(server: CoreServer, vite: ViteDevServer) {
 		try {
 			const cookies = req.headers['Cookie'] ?? '';
 
-			const { status, location, html, setCookies } =
-				await server.serverModRender(
-					serverMod,
-					fullUrl,
-					template,
+			const { status, location, html, setCookies } = await modRender(
+				serverMod,
+				{
+					url: fullUrl,
+					htmlTemplate: template,
+					ssrManifest: {},
 					acceptLang,
-					Array.isArray(cookies) ? cookies.join(';') : cookies,
-				);
+					endpoint: env.endpointUrl,
+					craftWeb: env.craftWebUrl,
+					viteEnv: env.viteEnv,
+					cookies: Array.isArray(cookies)
+						? cookies.join(';')
+						: cookies,
+				},
+			);
 
 			if (setCookies) {
 				res.setHeader('Set-Cookie', setCookies);
@@ -219,13 +173,15 @@ export async function serveVite(server: CoreServer, vite: ViteDevServer) {
 		if (error.status !== 503 && process.env.NODE_ENV === 'development')
 			return next(thrownError);
 
-		const { status, html } = await server.serverModRenderError(
-			serverMod,
-			error,
-			fullUrl,
-			template,
+		const { status, html } = await modRenderError(serverMod, error, {
+			url: fullUrl,
+			htmlTemplate: template,
+			ssrManifest: {},
+			endpoint: env.endpointUrl,
+			craftWeb: env.craftWebUrl,
+			viteEnv: env.viteEnv,
 			acceptLang,
-		);
+		});
 
 		res.statusCode = status;
 		res.setHeader('Content-Type', 'text/html');
@@ -233,80 +189,80 @@ export async function serveVite(server: CoreServer, vite: ViteDevServer) {
 	});
 }
 
-export async function serveExpress(server: CoreServer, app: Express) {
-	app.use(express.static('./dist/public'));
+// export async function serveExpress(server: CoreServer, app: Express) {
+// 	app.use(express.static('./dist/public'));
 
-	const ssrManifest = await readFile('./dist/ssr-manifest.json');
-	server.ssrManifest = JSON.parse(ssrManifest);
+// 	const ssrManifest = await readFile('./dist/ssr-manifest.json');
+// 	server.ssrManifest = JSON.parse(ssrManifest);
 
-	const template = await readFile('./dist/index.html');
+// 	const template = await readFile('./dist/index.html');
 
-	app.use('*', async (req, res, next) => {
-		const url = req.originalUrl;
-		const fullUrl = req.protocol + '://' + req.get('host') + url;
-		const acceptLang = req.get('accept-language') ?? null;
-		let serverMod = null;
-		let thrownError: any = null;
-		try {
-			const distServer = path.resolve('./dist/server.js');
-			serverMod = await import(distServer);
-		} catch (e) {
-			return next(e);
-		}
-		try {
-			// render app html
-			const { status, location, html, setCookies } =
-				await server.serverModRender(
-					serverMod,
-					fullUrl,
-					template,
-					acceptLang,
-					req.get('Cookie') ?? '',
-				);
+// 	app.use('*', async (req, res, next) => {
+// 		const url = req.originalUrl;
+// 		const fullUrl = req.protocol + '://' + req.get('host') + url;
+// 		const acceptLang = req.get('accept-language') ?? null;
+// 		let serverMod = null;
+// 		let thrownError: any = null;
+// 		try {
+// 			const distServer = path.resolve('./dist/server.js');
+// 			serverMod = await import(distServer);
+// 		} catch (e) {
+// 			return next(e);
+// 		}
+// 		try {
+// 			// render app html
+// 			const { status, location, html, setCookies } =
+// 				await server.serverModRender(
+// 					serverMod,
+// 					fullUrl,
+// 					template,
+// 					acceptLang,
+// 					req.get('Cookie') ?? '',
+// 				);
 
-			if (setCookies) {
-				res.append('Set-Cookie', setCookies);
-			}
+// 			if (setCookies) {
+// 				res.append('Set-Cookie', setCookies);
+// 			}
 
-			if (status === 301 || status === 302) {
-				res.redirect(status, location ?? '');
-				return;
-			}
+// 			if (status === 301 || status === 302) {
+// 				res.redirect(status, location ?? '');
+// 				return;
+// 			}
 
-			// Send the rendered HTML back
-			res.status(status).set({ 'Content-Type': 'text/html' }).end(html);
-			return;
-		} catch (e: any) {
-			console.log('error', e);
+// 			// Send the rendered HTML back
+// 			res.status(status).set({ 'Content-Type': 'text/html' }).end(html);
+// 			return;
+// 		} catch (e: any) {
+// 			console.log('error', e);
 
-			if (typeof serverMod.renderError !== 'function') return next(e);
+// 			if (typeof serverMod.renderError !== 'function') return next(e);
 
-			thrownError = e;
-		}
+// 			thrownError = e;
+// 		}
 
-		// in the case of an error let's try to render a nice Error Page
-		const error = {
-			status: 500,
-			message: thrownError.message,
-		};
+// 		// in the case of an error let's try to render a nice Error Page
+// 		const error = {
+// 			status: 500,
+// 			message: thrownError.message,
+// 		};
 
-		if (typeof thrownError.__isGraphQlError__ === 'function')
-			error.status = thrownError.status();
+// 		if (typeof thrownError.__isGraphQlError__ === 'function')
+// 			error.status = thrownError.status();
 
-		if (error.status !== 503 && process.env.NODE_ENV === 'development')
-			return next(thrownError);
+// 		if (error.status !== 503 && process.env.NODE_ENV === 'development')
+// 			return next(thrownError);
 
-		const { status, html } = await server.serverModRenderError(
-			serverMod,
-			error,
-			fullUrl,
-			template,
-			acceptLang,
-		);
+// 		const { status, html } = await server.serverModRenderError(
+// 			serverMod,
+// 			error,
+// 			fullUrl,
+// 			template,
+// 			acceptLang,
+// 		);
 
-		res.status(status).set({ 'Content-Type': 'text/html' }).end(html);
-	});
-}
+// 		res.status(status).set({ 'Content-Type': 'text/html' }).end(html);
+// 	});
+// }
 
 /**
  * Parses an .env file
@@ -325,4 +281,49 @@ function readEnv(fileCtn: string): Map<string, string> {
 	}
 
 	return map;
+}
+
+function requestToWebRequest(
+	baseUrl: string,
+	nodeReq: Connect.IncomingMessage,
+): Request {
+	const method = nodeReq.method ?? 'GET';
+
+	let body;
+	if (method !== 'GET' && method !== 'HEAD') {
+		body = Readable.toWeb(nodeReq) as BodyInit;
+	}
+
+	const url = baseUrl + (nodeReq.originalUrl ?? nodeReq.url);
+
+	// 4. Construct a new Request
+	return new Request(url, {
+		method,
+		headers: nodeReq.headers as Record<string, string>,
+		body,
+	});
+}
+
+export async function webResponseToResponse(
+	webResponse: Response,
+	nodeRes: http.ServerResponse,
+): Promise<void> {
+	nodeRes.statusCode = webResponse.status;
+	nodeRes.statusMessage = webResponse.statusText || '';
+
+	for (const [key, value] of webResponse.headers.entries()) {
+		nodeRes.setHeader(key, value);
+	}
+
+	if (!webResponse.body) {
+		nodeRes.end();
+		return;
+	}
+
+	const nodeStream = Readable.fromWeb(webResponse.body as any);
+	nodeStream.pipe(nodeRes);
+
+	nodeStream.on('error', err => {
+		nodeRes.destroy(err);
+	});
 }
